86ce4798334118ae5c582043e005304061f0a0d8
Danyal Sheikh <danyal.a.sheikh@gmail.com>
2016-08-13 22:23:28 -0500

    Switched to prefs

--- sample.py
+++ sample.py
@@ -6,28 +6,46 @@ Listener will then store tweets into a database for
 further parsing and storage.
 '''
 
+import json
+
 '''
 Using birdy library for python-twitter interface
 '''
 from birdy.twitter import StreamClient
 from pymongo import MongoClient
 
-client = StreamClient('consumer_key',
-    'consumer_secret',
-    'access_token',
-    'access_secret')
+'''
+Open prefs file with stored parameters and load file
+'''
+prefs_file = open('prefs')
+prefs = json.load(prefs_file)
+prefs_file.close()
+
+'''
+Initialize streaming client with prefs
+'''
+tokens = prefs['tokens']
+client = StreamClient(str(tokens['consumer_key']),
+    str(tokens['consumer_secret']),
+    str(tokens['access_token']),
+    str(tokens['access_secret']))
 
 '''
 Connecting to mongodb and setting up a client interface
 '''
 mclient = MongoClient()
-db = mclient['test_db']
+db_name = str(prefs['mongo']['db_name'])
+db = mclient[db_name]
 
-response = client.stream.statuses.filter.post(track='olympics')
+'''
+Begin tracking using tracking expression provided in prefs
+'''
+track_exp = str(prefs['track_exp'])
+response = client.stream.statuses.filter.post(track=track_exp)
 
 '''
 Iterates over the data returned in the response stream
-and stores it into the mongodb database for parsing
+and stores the data into a mongodb database for parsing
 '''
 for data in response.stream():
     db.posts.insert_one(data)
